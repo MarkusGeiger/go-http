4540910fcec2459d2ccf87ee271e1d1646d9a6e4
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2022-05-25 00:21:31 +0200

    Fetch data from API, delete inactive users

--- program.go
+++ program.go
@@ -1,7 +1,66 @@
+// go/src/http-client/main.go
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type users struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	c := http.Client{Timeout: time.Duration(1) * time.Second}
+	resp, err := c.Get("https://gorest.co.in/public/v2/users")
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return
+	}
+
+	fmt.Printf("Body : %s", body)
+
+	users := []users{}
+	err2 := json.Unmarshal([]byte(body), &users)
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	fmt.Println("\nUsers:")
+	fmt.Println(users)
+
+	for _, user := range users {
+		if user.Status == "inactive" {
+			targetUrl := fmt.Sprint("https://gorest.co.in/public/v2/users/", user.Id)
+			req, err := http.NewRequest("DELETE", targetUrl, nil)
+			if err != nil {
+				fmt.Printf("error %s", err)
+				continue
+			}
+			resp, err := c.Do(req)
+			if err != nil {
+				fmt.Printf("error %s", err)
+				continue
+			}
+			//defer resp.Body.Close()
+			//body, err := ioutil.ReadAll(resp.Body)
+			//if err != nil {
+			//	fmt.Printf("error %s", err)
+			//	continue
+			//}
+			fmt.Printf("\n%s Status: %s", targetUrl, resp.Status)
+		}
+	}
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+# Template
 https://www.docker.com/blog/containerize-your-go-developer-environment-part-1/
 
 bash: export DOCKER_BUILDKIT=1
@@ -7,3 +8,6 @@ cmd: set DOCKER_BUILDKIT=1
 make
 make PLATFORM=windows/amd64
 make PLATFORM=linux/arm64
+
+# Implementation
+https://blog.logrocket.com/configuring-the-go-http-client/
